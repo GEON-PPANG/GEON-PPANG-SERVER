2d8c708dede256523130351dad617d7055539715
LimSeungHa <seungha.lim@kurlycorp.com>
2023-06-23 19:56:51 +0900

    INITIAL COMMIT

---
+++ src/main/java/com/org/gunbbang/GunbbangApplication.java
@@ -0,0 +1,13 @@
+package com.org.gunbbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GunbbangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GunbbangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/gunbbang/GunbbangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.gunbbang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GunbbangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
