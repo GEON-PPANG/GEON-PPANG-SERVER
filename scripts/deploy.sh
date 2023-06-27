fc780cf9ab90020c324a6fd279311ea5785ad631
Seungha Lim <89137120+seunghaLim@users.noreply.github.com>
2023-06-28 01:44:46 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -48,36 +48,36 @@ fi
 echo "> $IDLE_PROFILE 배포"
 nohup java -jar -Duser.timezone=Asia/Seoul -Dspring.profiles.active=$IDLE_PROFILE $IDLE_APPLICATION_PATH >> /home/ubuntu/app/nohup.out 2>&1 &
 
-# echo "> $IDLE_PROFILE 10초 후 Health check 시작"
-# echo "> curl -s http://localhost:$IDLE_PORT/health "
-# sleep 10
+echo "> $IDLE_PROFILE 10초 후 Health check 시작"
+echo "> curl -s http://localhost:$IDLE_PORT/health "
+sleep 10
 
-# health check부분
-# for retry_count in {1..10}
-# do
-#   response=$(curl -s http://localhost:$IDLE_PORT/actuator/health)
-#   up_count=$(echo $response | grep 'UP' | wc -l)
+health check부분
+for retry_count in {1..10}
+do
+  response=$(curl -s http://localhost:$IDLE_PORT/actuator/health)
+  up_count=$(echo $response | grep 'UP' | wc -l)
 
-#   if [ $up_count -ge 1 ]
-#   then # $up_count >= 1 ("UP" 문자열이 있는지 검증)
-#       echo "> Health check 성공"
-#       break
-#   else
-#       echo "> Health check의 응답을 알 수 없거나 혹은 status가 UP이 아닙니다."
-#       echo "> Health check: ${response}"
-#   fi
+  if [ $up_count -ge 1 ]
+  then # $up_count >= 1 ("UP" 문자열이 있는지 검증)
+      echo "> Health check 성공"
+      break
+  else
+      echo "> Health check의 응답을 알 수 없거나 혹은 status가 UP이 아닙니다."
+      echo "> Health check: ${response}"
+  fi
 
-#   if [ $retry_count -eq 10 ]
-#   then
-#     echo "> Health check 실패. "
-#     echo "> Nginx에 연결하지 않고 배포를 종료합니다."
-#     exit 1
-#   fi
+  if [ $retry_count -eq 10 ]
+  then
+    echo "> Health check 실패. "
+    echo "> Nginx에 연결하지 않고 배포를 종료합니다."
+    exit 1
+  fi
 
-#   echo "> Health check 연결 실패. 재시도..."
-#   sleep 10
-# done
+  echo "> Health check 연결 실패. 재시도..."
+  sleep 10
+done
 
-# echo "> 스위칭"
-# sleep 10
-# /home/ubuntu/app/nonstop/switch.sh
+echo "> 스위칭"
+sleep 10
+/home/ubuntu/app/nonstop/switch.sh
