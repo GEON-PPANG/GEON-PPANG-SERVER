dd4faaf5810148aadade7d93fa614865702e70f4
Seungha Lim <89137120+seunghaLim@users.noreply.github.com>
2023-07-11 17:09:37 +0900

    Update SecurityConfig.java

--- api/src/main/java/com/org/gunbbang/config/SecurityConfig.java
+++ api/src/main/java/com/org/gunbbang/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
 
                 // 아이콘, css, js 관련
                 // 기본 페이지, css, image, js 하위 폴더에 있는 자료들은 모두 접근 가능, h2-console에 접근 가능
-                .antMatchers("/","/css/**","/images/**","/js/**","/favicon.ico","/h2-console/**").permitAll()
+                .antMatchers("/","/css/**","/images/**","/js/**","/favicon.ico","/h2-console/**", "/health", "/profile").permitAll()
                 .antMatchers("/member/signup").permitAll() // 회원가입은 모든 리소스에 접근 가능
                 .anyRequest().authenticated();
 
