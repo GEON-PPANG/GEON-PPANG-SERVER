472562d93e71293e6081c135e2e0fd95fb0a33ca
sung-silver <jinna0319@gmail.com>
2023-07-13 23:18:25 +0900

    [FIX/#42] 건빵집 상세 조회 확인 ResponseDto 형식 맞춰라 건빵집 상세 조회 api에서 ResponseDto에 주소가 빠져있어 수정했습니다

--- api/src/main/java/com/org/gunbbang/controller/DTO/response/BakeryDetailResponseDto.java
+++ api/src/main/java/com/org/gunbbang/controller/DTO/response/BakeryDetailResponseDto.java
@@ -12,18 +12,20 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @SuperBuilder
 public class BakeryDetailResponseDto extends BaseBakeryResponseDTO {
-    private BreadTypeResponseDto breadTypeResponseDto;
+    private BreadTypeResponseDto breadType;
     private String homepage;
+    private String address;
     private String openingTime;
     private String closedDay;
     private String phoneNumber;
     private List<MenuResponseDto> menuList;
 
-    public BakeryDetailResponseDto(Long bakeryId, String bakeryName, String bakeryPicture, Boolean isHACCP, Boolean isVegan, Boolean isNonGMO, BreadTypeResponseDto breadTypeResponseDto, String firstNearStation, String secondNearStation, Boolean isBooked, int bookmarkCount, String homepage, String openingTime, String closedDay, String phoneNumber, List<MenuResponseDto> menuList) {
+    public BakeryDetailResponseDto(Long bakeryId, String bakeryName, String bakeryPicture, Boolean isHACCP, Boolean isVegan, Boolean isNonGMO, BreadTypeResponseDto breadType, String firstNearStation, String secondNearStation, Boolean isBooked, int bookmarkCount, String homepage, String address, String openingTime, String closedDay, String phoneNumber, List<MenuResponseDto> menuList) {
         super(bakeryId, bakeryName, bakeryPicture, isHACCP, isVegan,
                 isNonGMO, firstNearStation, secondNearStation, isBooked, bookmarkCount);
-        this.breadTypeResponseDto = breadTypeResponseDto;
+        this.breadType = breadType;
         this.homepage = homepage;
+        this.address = address;
         this.openingTime = openingTime;
         this.closedDay = closedDay;
         this.phoneNumber = phoneNumber;
