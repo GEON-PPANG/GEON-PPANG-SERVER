21e3c3cd05d78fa98f21fe0f4c93a76432919a16
sung-silver <jinna0319@gmail.com>
2023-07-17 22:29:07 +0900

    [FIX/#77] 리뷰 좋아요 선택시 리뷰키워드를 선택하지 않아도 저장되는 로직을 수정해라

--- api/src/main/java/com/org/gunbbang/controller/ReviewController.java
+++ api/src/main/java/com/org/gunbbang/controller/ReviewController.java
@@ -1,8 +1,10 @@
 package com.org.gunbbang.controller;
 
+import com.org.gunbbang.BadRequestException;
 import com.org.gunbbang.common.dto.ApiResponse;
 import com.org.gunbbang.controller.DTO.request.ReviewRequestDTO;
 import com.org.gunbbang.controller.DTO.response.ReviewDetailResponseDTO;
+import com.org.gunbbang.errorType.ErrorType;
 import com.org.gunbbang.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import com.org.gunbbang.errorType.SuccessType;
@@ -22,6 +24,9 @@ public class ReviewController {
     public ApiResponse createReview(@PathVariable("bakeryId") @Valid final Long bakeryId, @RequestBody @Valid final ReviewRequestDTO request) {
         Long reviewId = reviewService.createReview(bakeryId, request);
         if (request.getIsLike()) {
+            if(request.getKeywordList().isEmpty()){
+                throw new BadRequestException(ErrorType.REQUEST_KEYWORDLIST_VALIDATION_EXCEPTION);
+            }
             reviewService.createReviewRecommendKeyword(request.getKeywordList(), reviewId);
         }
         return ApiResponse.success(SuccessType.CREATE_REVIEW_SUCCESS);
