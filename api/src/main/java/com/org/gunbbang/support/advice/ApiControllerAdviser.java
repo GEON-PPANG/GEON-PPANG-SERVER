3a44ea9578ad0a62b0e1d7bc095c17efc08baced
seunghaLim <tmdgk490255@gmail.com>
2023-07-19 17:21:47 +0900

    [FIX] 바인딩 에러에 대해 공통 응답 규격으로 반환하도록 변경

--- api/src/main/java/com/org/gunbbang/support/advice/ApiControllerAdviser.java
+++ api/src/main/java/com/org/gunbbang/support/advice/ApiControllerAdviser.java
@@ -2,12 +2,58 @@ package com.org.gunbbang.support.advice;
 
 import com.org.gunbbang.HandleException;
 import com.org.gunbbang.common.dto.ApiResponse;
+import com.org.gunbbang.errorType.ErrorType;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.Objects;
 
 @RestControllerAdvice
 public class ApiControllerAdviser {
+
+    // 요청이 스프링 MVC 유효성 검사에 실패한 경우
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ApiResponse handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        FieldError fieldError = Objects.requireNonNull(e.getFieldError());
+        return ApiResponse.error(
+                ErrorType.REQUEST_VALIDATION_EXCEPTION,
+                String.format("%s. (%s)", fieldError.getDefaultMessage(), fieldError.getField()));
+    }
+
+    // 필요한 요청 파라미터 값이 오지 않은 경우
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    protected ApiResponse handleMissingServletRequestParameterException(final MissingServletRequestParameterException e) {
+        return ApiResponse.error(
+                ErrorType.NO_REQUEST_PARAMETER_EXCEPTION,
+                String.format("%s. (%s)", ErrorType.NO_REQUEST_PARAMETER_EXCEPTION.getMessage(), e.getParameterName()));
+    }
+
+    // 요청 필드의 타입이 일치하지 않은 경우
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    protected ApiResponse handleMethodArgumentTypeMismatchException(final MethodArgumentTypeMismatchException e) {
+        return ApiResponse.error(
+                ErrorType.PARAMETER_TYPE_MISMATCH_EXCEPTION,
+                String.format("%s. (%s)", ErrorType.PARAMETER_TYPE_MISMATCH_EXCEPTION.getMessage(), e.getName()));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BindException.class)
+    protected ApiResponse handleBindingException(final BindException e) {
+        return ApiResponse.error(
+                ErrorType.REQUEST_BIND_EXCEPTION,
+                String.format("%s. (%s)", ErrorType.REQUEST_BIND_EXCEPTION, e.getBindingResult()));
+    }
     @ExceptionHandler(HandleException.class)
     protected ResponseEntity<ApiResponse> handleCustomException(HandleException e) {
         return ResponseEntity.status(e.getHttpStatus())
