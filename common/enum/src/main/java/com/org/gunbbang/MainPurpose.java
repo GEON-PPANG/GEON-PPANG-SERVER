b877a057df7b526d79a1a5e3ca1fe3608e245a8c
seunghaLim <tmdgk490255@gmail.com>
2023-08-15 07:19:16 +0900

    [FEAT] 회원가입 시 필터 선택 안했을 때 들어가는 디폴트 값을 설정하라

--- common/enum/src/main/java/com/org/gunbbang/MainPurpose.java
+++ common/enum/src/main/java/com/org/gunbbang/MainPurpose.java
@@ -8,7 +8,9 @@ import lombok.Getter;
 public enum MainPurpose {
   HEALTH("건강/체질"),
   DIET("맛/디이어트"),
-  VEGAN("비건/채식지향");
+  VEGAN("비건/채식지향"),
+  NONE("주목적 선택 안함"),
+  ;
 
   private String desc;
 }
