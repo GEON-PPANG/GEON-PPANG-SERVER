a3dd8da4dea0ff1e0a8f750975e8b257d5400009
sung-silver <jinna0319@gmail.com>
2023-08-26 00:25:06 +0900

    [FEAT] CustomOAuth2User 클래스 생성해라

---
+++ api/src/main/java/com/org/gunbbang/login/CustomOAuth2User.java
@@ -0,0 +1,25 @@
+package com.org.gunbbang.login;
+
+import com.org.gunbbang.Role;
+import java.util.Collection;
+import java.util.Map;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+
+@Getter
+public class CustomOAuth2User extends DefaultOAuth2User {
+  private String email;
+  private Role role;
+
+  public CustomOAuth2User(
+      Collection<? extends GrantedAuthority> authorities,
+      Map<String, Object> attributes,
+      String nameAttributeKey,
+      String email,
+      Role role) {
+    super(authorities, attributes, nameAttributeKey);
+    this.email = email;
+    this.role = role;
+  }
+}
