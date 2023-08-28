5e55b0140928981ef6eb68ce68eb056b5cfa7013
seunghaLim <tmdgk490255@gmail.com>
2023-08-28 23:54:11 +0900

    [CHORE] codemetrics 적용 후 리팩토링

--- api/src/main/java/com/org/gunbbang/service/ReviewService.java
+++ api/src/main/java/com/org/gunbbang/service/ReviewService.java
@@ -201,10 +201,12 @@ public class ReviewService {
   }
 
   private static boolean isFilterNotSelected(Member foundMember) {
-    return foundMember.getBreadType().getIsGlutenFree() == false
-        && foundMember.getBreadType().getIsNutFree() == false
-        && foundMember.getBreadType().getIsSugarFree() == false
-        && foundMember.getBreadType().getIsVegan() == false
+    BreadType foundBreadType = foundMember.getBreadType();
+
+    return !foundBreadType.getIsGlutenFree()
+        && !foundBreadType.getIsNutFree()
+        && !foundBreadType.getIsSugarFree()
+        && !foundBreadType.getIsVegan()
         && foundMember.getMainPurpose() == MainPurpose.NONE;
   }
 
