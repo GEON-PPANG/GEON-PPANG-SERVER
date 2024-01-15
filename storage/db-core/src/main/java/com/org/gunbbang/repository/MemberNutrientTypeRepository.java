48ea77a7815694083af9796cb4d876745a24ab36
sung-silver <jinna0319@gmail.com>
2024-01-16 01:11:48 +0900

    [FIX] MemberNutrientTypeRepository 수정

--- storage/db-core/src/main/java/com/org/gunbbang/repository/MemberNutrientTypeRepository.java
+++ storage/db-core/src/main/java/com/org/gunbbang/repository/MemberNutrientTypeRepository.java
@@ -3,6 +3,7 @@ package com.org.gunbbang.repository;
 import com.org.gunbbang.entity.Member;
 import com.org.gunbbang.entity.MemberNutrientType;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -18,4 +19,7 @@ public interface MemberNutrientTypeRepository extends JpaRepository<MemberNutrie
   @Modifying
   @Query("delete from MemberNutrientType mnt where mnt.member=:member")
   void deleteAllByMember(Member member);
+
+  @Query("select mnt from MemberNutrientType mnt where mnt.member.memberId = :memberId")
+  Optional<MemberNutrientType> findByMemberId(Long memberId);
 }
