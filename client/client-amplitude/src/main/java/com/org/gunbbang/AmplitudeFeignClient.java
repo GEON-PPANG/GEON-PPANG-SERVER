fff60ad95fdfd15bec32fce717dbd30a1b07aec5
seunghaLim <tmdgk490255@gmail.com>
2024-09-22 03:58:10 +0900

    [CHORE] DEV CI 및 CD 스크립트 변경

--- client/client-amplitude/src/main/java/com/org/gunbbang/AmplitudeFeignClient.java
+++ client/client-amplitude/src/main/java/com/org/gunbbang/AmplitudeFeignClient.java
@@ -16,7 +16,7 @@ public interface AmplitudeFeignClient {
   @PostMapping(value = "/2/httpapi", consumes = MediaType.APPLICATION_JSON_VALUE)
   HttpV2ResponseDTO uploadRequest(@RequestBody @Valid HttpV2RequestDTO request);
 
-  // TODO!!: 안될 것 같은데
+  // TODO: 응답값 제대로 작동되는지 추후 확인 필요
   @PostMapping(value = "/identify", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
   void identifyUserProperty(@RequestBody String request);
 }
